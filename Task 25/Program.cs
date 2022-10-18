77cd6a201fc3023dc771ca1b528e9201f580ea30
Dina <dinalukoshkina@mail.ru>
2022-10-18 11:09:39 +0200

    Task 25 solved

---
+++ Task 25/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите цикл, который принимает на вход два число A и В 
+// и возводит число А в натуральную степень В
+Console.Clear();
+Console.Write("Введите А: ");
+int a = int.Parse(Console.ReadLine()!);
+Console.Write("Введите B: ");
+int b = int.Parse(Console.ReadLine()!);
+void NC(int a, int b)
+{
+    int result = 1;
+    for (int i = 1; i <= b; i++)
+    {
+        result = result * a;
+    }
+    Console.WriteLine(result);
+}
+NC(a,b);
