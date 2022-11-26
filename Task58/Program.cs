c9c8116fed2eb3c33d20066fc1b87b2a53f245ae
Dina <dinalukoshkina@mail.ru>
2022-11-26 23:01:08 +0100

    Task 58 solved

---
+++ Task58/Program.cs
@@ -0,0 +1,29 @@
+﻿//Задайте две матрицы. 
+//Напишите программу, которая будет находить произведение двух матриц.
+//Например, даны 2 матрицы:
+//2 4 | 3 4
+//3 2 | 3 3
+//Результирующая матрица будет:
+//18 20
+//15 18
+
+
+public class Program
+{
+    public static void Main()
+    {
+
+        writeDistance(5, 9, 2, 4);
+    }
+
+    public static void writeDistance(double k1, double k2, double b1, double b2)
+    {
+        double x, y;
+
+        x = (b1 - b2) / (k2 - k1);
+        y = k1 * x + b1;
+
+        Console.WriteLine("x: {0}, y: {1}", x, y);
+    }
+
+}
