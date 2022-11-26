266fdd7ad7a6636acc422fa5b208409501abf6bb
Dina <dinalukoshkina@mail.ru>
2022-11-26 23:17:57 +0100

    Task 58 solved

--- Task58/Program.cs
+++ Task58/Program.cs
@@ -13,17 +13,40 @@ public class Program
     public static void Main()
     {
 
-        writeDistance(5, 9, 2, 4);
+        //int[,] arr1 = { { 2, 4},
+        //       { 3, 2 } };
+        //  int[,] arr2 = { { 3, 4 },
+        //         { 3, 3} };
+
+        int[,] arr1 = { { 2, 0, -1},
+             { 0, -2, 2 } };
+        int[,] arr2 = { { 4, 1, 0 },
+             { 3, 2, 1},
+             { 0, 1, 0},
+            };
+
+        multiplyArrays(arr1, arr2);
+
     }
 
-    public static void writeDistance(double k1, double k2, double b1, double b2)
+    public static void multiplyArrays(int[,] arr1, int[,] arr2)
     {
-        double x, y;
 
-        x = (b1 - b2) / (k2 - k1);
-        y = k1 * x + b1;
+        int[,] result = new int[arr1.GetLength(0), arr2.GetLength(0)];
 
-        Console.WriteLine("x: {0}, y: {1}", x, y);
+        for (int i = 0; i < arr1.GetLength(0); i++)
+        {
+            for (int j = 0; j < arr1.GetLength(1); j++)
+            {
+                result[i, j] = 0;
+                for (int k = 0; k < arr1.GetLength(1); k++)
+                {
+                    result[i, j] += arr1[i, k] * arr2[k, j];
+                }
+                Console.Write(result[i, j] + " ");
+            }
+            Console.WriteLine();
+        }
     }
 
 }
